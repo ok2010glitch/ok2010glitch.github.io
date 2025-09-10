00532bee71ea70031e89b040105390ea0156d7f7
ok2010glitch <7011322@spsd.sk.ca>
2025-09-10 13:16:34 -0600

    01 first demo

---
+++ 01 first program/sketch.js
@@ -0,0 +1,18 @@
+// Project Title
+// Syed Saad HUssain
+// September 9, 2025
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() { // runs ONCE at the start
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() { // runs OVER and OVER
+                  //targeting 60 frames per second
+  //background(220);
+  fill (255,0,0)
+  circle(mouseX, mouseY,30)
+}
