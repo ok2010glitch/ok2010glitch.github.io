1e92dc6c30e26eb9579663b6d0ddc86423c464ed
ok2010glitch <7011322@spsd.sk.ca>
2025-09-10 14:11:06 -0600

    almost done it demo 01

--- 01 first program/sketch.js
+++ 01 first program/sketch.js
@@ -1,6 +1,6 @@
-// Project Title
+// Coordinates system
 // Syed Saad HUssain
-// September 9, 2025
+// September 10, 2025
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
@@ -8,11 +8,30 @@
 
 function setup() { // runs ONCE at the start
   createCanvas(windowWidth, windowHeight);
-}
+  print(windowWidth, windowHeight, width, height);
+} //windowwidth: current bower width
+  //width;  canvas width
 
 function draw() { // runs OVER and OVER
                   //targeting 60 frames per second
-  //background(220);
-  fill (255,0,0)
-  circle(mouseX, mouseY,30)
+  background(220);// draws a big solid rectangle(kinda grey)
+  fill (0,255,0);
+  circle(mouseX, mouseY,30); // 
 }
+function drawtwocirlces(){ //[ALT][SHFT][F] -> Autoformat
+  // draw the cirlces one at a fixed location
+  // and one at the mouse location
+
+  fill(0,255,0)//colors are abit like
+  // picking up a marker, or 
+  // filling a paintbrush wiht paint...
+  // R G B
+  circle(mouseX,mouseY,30);
+
+  fill(255,0,255);
+  circle(width/2,height/2, 100)
+
+
+
+}
+drawtwocicles()
