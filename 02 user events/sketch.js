fcd018990bc27d8d65c86c9c9c24439b3de876b4
ok2010glitch <7011322@spsd.sk.ca>
2025-09-12 14:10:51 -0600

    finshed demo
    cya Mr.Scott have a nice weekend

--- 02 user events/sketch.js
+++ 02 user events/sketch.js
@@ -3,46 +3,127 @@
 // September 11, 2025
 
 
-// global variables here..
- let circlecolor = false;
- // declaration     initialization
+// GLOBAL VARIABLES HERE...
+// can only work wiht "simple" data in this
+// section of the code. No system variables are
+// vaiable until in setup(), ... after canvas is made 
+ let circleColor =  false;
+ let currentColor = "yellow";
 
+
+let y = 300;
+let x;
+let tSize = 50;
+
+
+//  declaration    initialization
 
 function setup() {
   createCanvas(400, 400);
+  x = width/2;
+  
 }
 
 function draw() {
   background(220);
-  challenge();  //coordinats system challengea
+  challenge();  //coordinate system challenge
+  rect(x,300,60,30)
+  mouseReport();
+}
+function mouseReport(){
+  // inspect some of the built-ins (system variables)
+  // for working with the mouse
+  fill(0);
+  let src = mouseX + ", " + mouseY + ", " + mouseIsPressed;
+  textSize(tSize);
+  text(src, mouseX, mouseY);
+
+
+
+}
+function movement(){
+  //check for keyborad presses each frame
+  // and move the rectangle accordingly
+  
+
+  if (keyIsDown(UP_ARROW)) y + 5;
+  if (keyIsDown(DOWN_ARROW)) y - 5;
+  if (keyIsDown(LEFT_ARROW)) x - 5;
+  if (keyIsDown(RIGHT_ARROW)) x + 5;
 
 
 
   
 }
+movement()
 
-function Keypressed(){
-  // this is spcial EVENt function, gets
-  // automatically called anytime a keyword
-  // button is pressed
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function keyPressed(){
+  // this is special EVENT function, gets
+  // automatically called anytime a keyboard
+  // button is pressed.
+
+
+
+
   print("key was pressed");
-  circle =!circlecolor;
+  if(key == "g") currentcolor = "green";
+  else if (keycode === CONTROL) currentColor = "blue";
+  circleColor = !circleColor;
+  
+  // how to tell WHICH key was pressed???
 
-  // how to tell which key was pressed
 }
+keyPressed()
+
 function challenge(){
-  // draw 5 hollow circles, in 4 corners and
-  // center positin, 50 px in diameter each
-  nofill();
+  // draw 5 hollow circles, in 4 corners and 
+  // center position,  50px in diameter each
+  noFill();
 
-  if(cicrlecolor == true){ //circlecolor === true
-    fill(255,0,0)
+  if(circleColor){  //circleColor === true
+    fill(currentColor);
   }
-
   //5 circles
-  circle(0,0,50);
+  circle(0,0,50); 
   circle(width,0,50);
   circle(0,height,50);
   circle(width,height,50);
-  circle(width * 0.5, height * 0.5,50);
+  circle(width * 0.5,height * 0.5,50);
 }
