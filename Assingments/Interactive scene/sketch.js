91f08a25f70ffe80da80095f66820ddd990bf59d
ok2010glitch <7011322@spsd.sk.ca>
2025-09-18 21:52:58 -0600

    finished withe boats

--- Assingments/Interactive scene/sketch.js
+++ Assingments/Interactive scene/sketch.js
@@ -3,11 +3,14 @@
 // September 16, 2025
 
 // This code will have moving elements such as the protagonist
-
+// GLobal Variables
 let g = 425; // ground level
 let topl = 275; // height of the mountain 
+let xb = 212 // for the boat
+let boatspeed = 3 // You can control the speed of the boat here
+let x = 540
 function setup() {
-  createCanvas(540,540);
+  createCanvas(x,540);
   
   
 }
@@ -28,9 +31,15 @@ text(pwinMouseX, 150, 150 );
 fill("blue")
 text(pwinMouseY, 50, 50);
 
-mountains()
-
-
+mountains();
+lake();
+boat();
+if (keyIsDown(RIGHT_ARROW) && x - 30){
+  xb += boatspeed;
+}
+if (keyIsDown(LEFT_ARROW) && x > 30){
+  xb -= boatspeed;
+}
 }
 function mountains(){
   fill("grey")
@@ -38,7 +47,23 @@ function mountains(){
   triangle(50,g,160,topl-5,280,g)
   triangle(170,g,240,topl-8,360,g)
 }
+function lake(){
+  fill(85, 171, 199)
+  rect(0,g,540,50)
+}
 
+function boat(){
+  // This is Saad's boat
+  fill("brown")
+  arc(xb, 410, 60, 40, 0, PI, PIE);
+  
+  // Mass stick holding th eboat
+  rect(xb-2,380, 4,30)
+  
+  fill("white")
+  // sail of the boat
+  triangle(xb-26,400,xb,370,xb+26,400)
+}
  
 
   
