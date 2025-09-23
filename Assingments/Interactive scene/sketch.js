2d178f1aeafb77df761546dd4c08453791509089
ok2010glitch <7011322@spsd.sk.ca>
2025-09-22 22:01:32 -0600

    done with the challenge, need some extra elements and I am done

--- Assingments/Interactive scene/sketch.js
+++ Assingments/Interactive scene/sketch.js
@@ -10,43 +10,43 @@ let xb = 212; // for the boat
 let boatspeed = 2; // You can control the speed of the boat here
 let bc = "white"; // boat's sail color
 let suncolor = "yellow";
-//let colorl = color(85, 171, 199)
+let bgcolor;
+let xs = 390;
 // position of the sun (x coordinate)
-// let currentback = 0;
+let currentback = 0;
+//let cx = 275; // Cloud x coordinate 
 function setup() {
   createCanvas(540,540);
+  bgcolor = color(0, 181, 226);
+
   
   
 }
 
 function draw() {
-  
-   // 0, 181, 226 -> skyblue but brighter
-   // 1, 51, 63 -> darker
 
-  // took some help from chatgpt, the mapping part
-  let r = map(mouseY, 0, height, 0, 1);
-  let g = map(mouseY, 0, height, 181, 51);
-  let b = map(mouseY, 0, height, 226, 63);   
+
   
-  background(r,g,b);
+// 0, 181, 226 -> skyblue but brighter
+// 1, 51, 63 -> darker
+
+// took some help from chatgpt, (the mapping part)
+// let r = map(mouseY, 0, height, 0, 1);
+// let g = map(mouseY, 0, height, 181, 51);
+// let b = map(mouseY, 0, height, 226, 63);   
+colourChanging();
+
+background(bgcolor);
                                   
   
-  fill("green")
+fill("green")
 
-  noStroke()
-  rect(0, gl, 540, 115)
-  textSize(16);
-
-// Displays the x coordinate
-fill("red")
-text(pwinMouseX, 150, 150 );
-// displays the y coordinate
-fill("blue")
-text(pwinMouseY, 50, 50);
+noStroke()
+rect(0, gl, 540, 115)
+  
 
 
-//drawshape();
+
 
 sun();
 mountains();
@@ -54,14 +54,19 @@ lake();
 boat();
 cloud();
 
+// text for illustrator's name
+textSize(20)
+fill("black");
+text("Syed Saad Hussain", 350, 520)
+
+// Displays the coordinates on the screen
+fill("purple")
+textSize(16);
+text("X: " + mouseX + "  Y: " + mouseY, mouseX + 20, mouseY);
 
 
-if (mouseY < gl){
-  suncolor = "yellow";
-} else{
-  suncolor = color(246, 241, 213);
 
-}
+//ALL MOVEMENTS HERE
 
 // Moving the boat right left
 if (keyIsDown(RIGHT_ARROW) && (xb + 30) < 540){ // (xb + 30) < 540) --> for restricting the boat from going outside the canvas
@@ -76,41 +81,67 @@ if (keyCode === 67){ // pressing letter c will change the colour
 if (keyCode === 82){ // press r and it will reset the color
   bc = "white";
 }
+// Cloud movement
+// if (cx < 540 || cx > 0){
+//   cx += 0.7
+//   //  Makes the cloud go on an repetitive loop
+//   if(cx > 570){
+//     cx = -60
+//     cx += 0.7
 
-
+//   }
 
 }
 
 
-// function mousePressed(){
-//   if(mouseButton === CENTER){
-//     currentback++;
-//     if(currentback > 3){  // reset when > 3
-//       currentback = 0;  
-//     }
-//   }
-// }
 
 
 
-// function drawshape(){
-//   // inspect our state variable, and draw 1 and 4 possible 
-//   //option, depending on the current variable
-//   switch(currentback){
-//     case 0:
-//       lakecolor = color(40, 208, 138)
-//       break;
-//     case 1:
-//       lakecolor = color(230, 179, 157)
-//       break;
-//     case 2:
-//       lakecolor = color(114, 166, 158)
-//       break;
-//     case 3:
-//       lakecolor = color(154, 185, 184)
-//       break;
-// }
-// }
+
+function mousePressed(){
+  if(mouseButton === CENTER){
+    currentback++;
+    if(currentback > 3){  // reset when > 3
+      currentback = 0;  
+    }
+  }
+}
+
+
+
+function colourChanging(){
+  // inspect our state variable, and draw 1 and 4 possible 
+  //option, depending on the current variable
+  switch(currentback){
+    case 0:
+      bgcolor = color(47, 175, 181); // bright teal
+      suncolor = "yellow";
+      fill("yellow");
+      circle(xs, 75,75);
+      break;
+    case 1:
+       bgcolor = color(111, 196, 200); // light blue
+       suncolor = "yellow";
+       fill("yellow");
+      circle(xs, 75,75);
+      break;
+    case 2:
+      bgcolor = color(53, 116, 119); // dark blue
+      suncolor = color(200, 200, 255);
+      fill(53, 116, 119);
+      circle(xs + 10, 75, 75);
+      break;
+    case 3:
+      bgcolor = color(29, 66, 67); // Night time
+      suncolor = color(200, 200, 255);
+      fill(29, 66, 67);
+      circle(xs + 10, 75, 75);
+      break;
+    default:
+      bgcolor = color(0, 181, 226);
+      break;
+}
+}
 
 
 
@@ -121,7 +152,7 @@ function mountains(){
   triangle(170,gl,240,topl-8,360,gl)
 }
 function lake(){
-  fill(85, 171, 199)
+  fill("blue")
   rect(0,gl,540,50)
 }
 
@@ -139,14 +170,14 @@ function boat(){
  
 function cloud(){
   fill("white")
-  circle(275,138,45);
-  circle(305,138,45);
-  circle(286,126,45);
+  circle(mouseX,138,45);
+  circle(mouseX + 30,138,45);
+  circle(mouseX + 11,126,45);
 
 }
 
 function sun(){
 fill(suncolor)
-circle(390,75,75);
+circle(xs,75,75);
 }  
   
