c3b1c620a0c9cf081f40ce2f369e32e22135a362
ok2010glitch <7011322@spsd.sk.ca>
2025-09-24 14:06:30 -0600

    challenge part pending

---
+++ 07 Working with images/sketch.js
@@ -0,0 +1,45 @@
+// Working with images
+// and random() and noise()
+// Saad
+// Sep 24, 2025
+//
+
+let x1, y1, x2, y2;
+let d1, d2;
+let noiseTime = 5, noiseSpeed = 0.1;
+// "noiseSpeed" controls how connected
+// our random noise() values are:
+let minSize = 5; 
+let maxSize = 200;
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  x1 = width*0.3; y1 = height*0.3;
+  x2 = width*0.7; y2 = height*0.5;
+  frameRate(1000);
+
+}
+
+function draw() {
+  background(220);
+  randomCircle();
+  noiseCircle();
+}
+
+
+function noiseCircle(){
+  // draw a fixed circle with randoml
+  // changing (but smooth, hopefully!) diameters
+  d2 = noise(noiseTime); //yields value between 0-1
+  d2 = map(d2, 0,1, minSize, maxSize);
+  fill(255,50,150);
+  circle(x2, y2, d2);
+  noiseTime += noiseSpeed;
+}
+function randomCircle(){
+  //draw a fixed circle with randomly changing diameter
+  fill(50, 205, 50);
+  d1 = random(5,100);
+  circle(x1, y1, d1);
+}
