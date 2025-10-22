5fb3aa0fb385ef39a117fc4fecbbb7432537a47a
ok2010glitch <7011322@spsd.sk.ca>
2025-10-22 14:12:39 -0600

    almost done

--- 19 Cars Cars Cars Assignmnt/sketch.js
+++ 19 Cars Cars Cars Assignmnt/sketch.js
@@ -3,67 +3,105 @@
 // October 20, 2025
 
 // Global Variables
- let myVehicle;
- let customW = 900;
- let customH = 600;
+let e;
+let w;
+let customW = 900;
+let customH = 600;
 
- let westbound = [];
- let eastbound = [];
+let westbound = [];
+let eastbound = [];
 
 
 function setup() {
   createCanvas(customW, customH);
 
+
+  for (i = 0; i < 20; i++) {
+
+    e = new vehicle(customW / 2, random(100, 275), 0);
+    eastbound.push(e);
+
+    w = new vehicle(customW/2, random(customH, 475),2);
+    westbound.push(w);
+
+  }
 }
 
 function draw() {
+  randomSeed(1);
   background(255, 216, 161);
   drawRoad();
-  myVehicle = new vehicle(customW/2, customH/2);
-  myVehicle.cars();
-  myVehicle.trucks();
+  for(let e of eastbound){
+   e.display();
+   e.move(); 
+  }
+  for(let w of westbound){
+    w.display();
+    w.move();
+
+
+  }
+  
+
 }
 
-function drawRoad(){
-  fill("black")
-  rect(0,100, width, 400 );
+function drawRoad() {
+  fill("grey")
+  rect(0, 100, width, 400);
   fill("white");
-  for (i = 0; i < 1000; i = i + 30*2.5){
+  for (i = 0; i < 1000; i = i + 30 * 2.5) {
     rect(i, 300, 50, 5);
   }
 
 }
 
-class vehicle{
+class vehicle {
   //1. Constructor
-  constructor(x,y){
+  constructor(x, y, d) {
     this.x = x; this.y = y;
-    this.c = color(random(255));
-    this.speed = random(1,5);
-
-
+    this.c = color(random(255),random(255),random(255));
+    this.speed = random(1, 5);
+    this.d = d
+    this.type = int(random(0, 2));
+    this.truckW = int(5)
   }
   //methods
-  cars(){
-    fill("white");
-    rect(this.x + 1, this.y - 4, 10, 38 );
-    rect(this.x + 49, this.y - 4, 10, 38 );
-    fill("red");
-    rect(this.x, this.y, 60, 30 );
-    
-    
+
+
+  display() {
+
+    if (this.type === 1) {
+      fill("black");
+      rect(this.x + 1, this.y - 4, 10, 38);
+      rect(this.x + 49, this.y - 4, 10, 38);
+      fill(this.c);
+      rect(this.x, this.y, 60, 30);
+
+    }
+    // else if(this.type === 5){
+    //   rect(this.x, this.y, -65, 40);
+    //   rect(this.x - 65.5, this.y + 3, -30, 34)
+
+    // }
+    else {
+      fill(this.c);
+      rect(this.x, this.y, 65, 40);
+      rect(this.x + 65.5, this.y + 3, 30, 34)
+
+    }
   }
-  trucks(){
-    fill("purple");
-    rect(this.x, this.y,65,40 );
-    rect(this.x + 65.5, this.y +3, 34, 32)
-
-
-
+  move() {
+    if (this.d === 0) {
+      this.x += this.speed;
+      if(this.x > customW){
+        this.x = 0;
+      }
+    }
+    else if(this.d === 2){
+      this.x -= this.speed;
+      if(this.x < customW){
+        this.x = 915;
+      }
+    }
   }
-  
-  display(){
-
-  }
-
 }
