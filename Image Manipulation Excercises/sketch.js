1e0b89cd6885ddd9487e6a2d8ba3c2b0be329976
ok2010glitch <7011322@spsd.sk.ca>
2025-11-17 14:10:10 -0600

    Left with the mirror image

--- Image Manipulation Excercises/sketch.js
+++ Image Manipulation Excercises/sketch.js
@@ -5,14 +5,12 @@
 
 let myImage;
 
-function split(){
+function gcolor(){
   // use the single loop stategy
  for(let i = 0; i < pixels.length; i += 4){
-  let pixelIndex = (i / 4) % width;
-  if(pixelIndex > 300){
-    pixels[i] = 255;
+  let pixelIndex = (i /4) % width;
+  if(pixelIndex > width/2){
     pixels[i+1] = 0;
-    pixels[i+2] = 255;
   }
 
 
@@ -24,7 +22,7 @@ function majorityColor(){
     let r = pixels[i];
     let g = pixels[i+1];
     let b = pixels[i+2];
-    if(r > g   && r > b){
+    if(r > g && r > b){
       pixels[i] = 255;
       pixels[i+1] = 0;
       pixels[i+2] = 0;
@@ -39,12 +37,52 @@ function majorityColor(){
       pixels[i+1] = 0;
       pixels[i+2] = 255;
     }
+    else if(r === b || r === g){
+      pixels[i] = 255;
+      pixels[i+1] = 0;
+      pixels[i+2] = 0;
+    }
+  }
+}
+
+function imgThree(){
+  for(let i = 0; i < pixels.length; i +=4){
+    let r = pixels[i];
+    let g = pixels[i+1];
+    let b = pixels[i+2];
+    let avg = (r+g+b) / 3
+    if(avg >= 205 && avg <= 255){
+      pixels[i] = 170;
+      pixels[i+1] = 230;
+      pixels[i+2] = 220;
+    }
+    else if(avg >= 155 && avg <= 204){
+      pixels[i] = 105;
+      pixels[i+1] = 150;
+      pixels[i+2] = 210;
+    }
+    else if(avg >= 105 && avg <= 154){
+      pixels[i] = 120;
+      pixels[i+1] = 180;
+      pixels[i+2] = 60;
+    }
+    else if(avg >= 55 && avg <= 104){
+      pixels[i] = 130;
+      pixels[i+1] = 30;
+      pixels[i+2] = 130;
+    }
+    else if(avg >= 0 && avg <= 54){
+      pixels[i] = 90;
+      pixels[i+1] = 10;
+      pixels[i+2] = 50;
+    }
+
   }
 }
 async function setup() {
 
   pixelDensity(1);
-  myImage = await loadImage("assets/race.jpg");
+  myImage = await loadImage("assets/nuit.jpg");
   createCanvas(600, 600);
 
 }
@@ -53,8 +91,9 @@ function draw() {
   background(220);
   image(myImage,0,0);
   loadPixels(); // populate the pixels array
-  split();
+  // gcolor();
   // majorityColor();
+  imgThree();
   
   updatePixels();
 
