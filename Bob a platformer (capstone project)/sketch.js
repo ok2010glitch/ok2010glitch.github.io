ef2cd90ed4174d52a0ad2ab079d208446ead8dd1
ok2010glitch <7011322@spsd.sk.ca>
2025-12-03 22:09:02 -0600

    Collisons done, left with wall jump

--- Bob a platformer (capstone project)/sketch.js
+++ Bob a platformer (capstone project)/sketch.js
@@ -7,18 +7,28 @@
 
 // Global Variables
 let levels;
-
+let plat = []
 let player;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  player = new Bob(700,25);
+  player = new Bob(650,25);
+  plat.push(new platform(0,570,width,400));
+  plat.push(new platform(30,450,300,50));
+  plat.push(new platform(520,400,150,50));
+
+  
 }
 
 function draw() {
   background(220);
   player.body();
   player.gravity();
+  for(let p of plat){
+    p.create();
+  }
+  player.collisionPlatsA();
+  player.movement();
 
   }
 
@@ -26,15 +36,17 @@ class Bob{
   constructor(x,y){
     this.x = x;
     this.y = y;
+    this.size = 60;
     this.vy = 0; // velocity y
     this.g = 0.5; // gravity
     this.sx = 4; //speed for player
-    this.jumpP = -5; // jump power
+    this.jumpP = -10; // jump power
+    this.onGround = false;
   }
   body(){
     noStroke();
     fill(0, 150, 255);
-    square(this.x,this.y,60,5);
+    square(this.x,this.y,this.size,5);
     fill("white")
     square(this.x+10,this.y+10,15,5);
     square(this.x+35,this.y+10,15,5);
@@ -43,29 +55,67 @@ class Bob{
     square(this.x+40,this.y+15,5,2);
   }
   movement(){
-    if(keyIsPressed(RIGHT_ARROW)){
+    if(keyIsDown(RIGHT_ARROW)){
       this.x += this.sx;
     }
-    if(keyIsPressed(LEFT_ARROW)){
+    if(keyIsDown(LEFT_ARROW)){
       this.x -= this.sx;
     }
-  }
+    if(keyIsDown(UP_ARROW) && this.onGround){
+      this.vy = this.jumpP;
+      this.onGround = false;
+    }
+    if(this.x > width){
+      this.x = 0;
+    }
+}
   gravity(){
     this.y += this.vy;
     this.vy += this.g;
+    this.onGround = false;
 
   }
-  collisionPlats(){
+  collisionPlatsA(){
+    for(let p of plat){
+      if(
+        this.x + this.size > p.xp &&
+        this.x < p.xp + p.w 
+      ){
+        if(
+          this.y + this.size > p.yp &&
+          this.y + this.size < p.yp + 20 &&
+          this.vy >= 0
+        ){
+          this.y = p.yp - this.size;
+          this.vy = 0;
+          this.onGround = true;
+        }
+        if(
+          this.y < p.yp + p.h &&
+          this.y > p.yp && this.vy < 0
+        ){
+          this.y = p.yp + p.h;
+          this.vy = 0;
+
+        }
+      }
 
   }
 
 }
 
-class platformer{
-  constructor(x,y,width,height){
-    this.x = x;
-    this.y = y;
-    this.w = width;
-    this.h = height;
+}
+
+class platform{
+  constructor(x,y,w,h){
+    this.xp = x;
+    this.yp = y;
+    this.w = w;
+    this.h = h;
+  }
+  create(){
+    fill("black")
+    rect(this.xp,this.yp,this.w,this.h);
+
   }
 }
